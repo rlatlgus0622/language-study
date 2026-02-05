5ecfd68f72516909f343b95a2ba75edca40eee57
rlatlgus0622 <rlatlgus0622@gmail.com>
2026-02-05 23:38:37 +0900

    feat: implement even and odd generator using OOP

---
+++ languages/python/range-iterator.py
@@ -0,0 +1,37 @@
+class NumberRange:
+    def __init__(self, limit):
+        if not isinstance(limit, int):
+            raise TypeError("It is not integer.")
+        if limit < 0:
+            raise ValueError("It is not 양수")
+        self.limit = limit
+
+    def __iter__(self):
+        pass
+
+class EvenGen(NumberRange):
+    def __iter__(self):
+        for n in range(0, self.limit + 1, 2):
+            yield n
+
+class OddGen(NumberRange):
+    def __iter__(self):
+        for n in range(1, self.limit + 1, 2):
+            yield n
+
+# main
+try:
+    limit = int(input("Enter limit: "))
+    even_gen = EvenGen(limit)
+    odd_gen = OddGen(limit)
+    
+    for i in even_gen:
+        print(i, end=' ')
+    print("")
+    for i in odd_gen:
+        print(i, end=' ')
+
+except TypeError as e:
+    print(f"TypeError: {e}")
+except ValueError as e:
+    print(f"ValueError: {e}")
