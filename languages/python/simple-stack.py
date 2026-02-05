1a450c9e465a3299029d34763f356ad4be7b13ec
rlatlgus0622 <rlatlgus0622@gmail.com>
2026-02-05 22:45:15 +0900

    feat: implement simple stack

---
+++ languages/python/simple-stack.py
@@ -0,0 +1,24 @@
+class SimpleStack:
+    def __init__(self):
+        self.data = []
+
+    def pop(self):
+        try:
+            return self.data.pop()
+        except IndexError:
+            print("Error: Stack is empty")
+            return None
+        
+    def push(self, item):
+        #정수만 받기
+        if not isinstance(item, int): #isinstance 내장함수 사용
+            raise TypeError(f"Only integer. Yours: {item}")
+        self.data.append(item)
+    
+s1 = SimpleStack()
+s1.push(1)
+s1.push(2)
+print(s1.pop())
+print(s1.pop())
+print(s1.pop())
+
