dbae195de97a1eacf70d9bb148fdd24875a8d147
rlatlgus0622 <rlatlgus0622@gmail.com>
2026-02-17 22:50:23 +0900

    feat(stack): add peek method in SimpleStack class

--- languages/python/data-structures/simple_stack.py
+++ languages/python/data-structures/simple_stack.py
@@ -14,6 +14,11 @@ class SimpleStack:
         if not isinstance(item, int): #isinstance 내장함수 사용
             raise TypeError(f"Only integer. Yours: {item}")
         self.data.append(item)
+
+    def peek(self):
+        if self.data:
+            return self.data[-1]
+        return None
     
 s1 = SimpleStack()
 s1.push(1)
@@ -21,4 +26,5 @@ s1.push(2)
 print(s1.pop())
 print(s1.pop())
 print(s1.pop())
+print(s1.peek())
 
